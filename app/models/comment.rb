0cd449af06c26c39903360f2babe7e1486211128
Nihar Patel <niharpatel52@gmail.com>
2025-08-22 11:30:45 -0400

    Update Comment model to accept/create new/existing users when commenting

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,9 @@ class Comment < ApplicationRecord
   belongs_to :post
   accepts_nested_attributes_for :user
 
+  def user_attributes=(attributes)
+    username = attributes[:username]
+    self.user = User.find_or_create_by(username: username) if username.present?
+  end
+
 end
