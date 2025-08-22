bfa3e185976176ef36c42342b90c98800ef4d8c2
Nihar Patel <niharpatel52@gmail.com>
2025-08-22 10:27:25 -0400

    Add ability to create posts with no categories

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,8 +6,10 @@ class Post < ApplicationRecord
 
   def categories_attributes=(category_attributes)
     category_attributes.values.each do |category_attribute|
-      category = Category.find_or_create_by(category_attribute)
-      self.categories << category
+      if category_attribute[:name].present?
+        category = Category.find_or_create_by(category_attribute)
+        self.categories << category
+      end
     end
   end
 end
