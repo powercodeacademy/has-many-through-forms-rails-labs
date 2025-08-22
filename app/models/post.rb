dd18fff172c41600202b0394533f60d67e203354
Kyle <kylegranthousel@gmail.com>
2025-08-22 10:26:43 -0400

    Pass all tests

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,13 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :users, through: :comments
 
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      name = category_attribute[:name].to_s.strip
+      next if name.blank?
 
+      category = Category.find_or_create_by(name: name)
+      categories << category unless categories.include?(category)
+    end
+  end
 end
